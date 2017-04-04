d6fa63651131bd66ca9f9ed5ce1b791c3f1513ed
Ashvin chauhan <ashvin.tps@gmail.com>
2017-04-04 12:41:31 +0530

    Remove duplicate migration of users_client_types

--- db/schema.rb
+++ db/schema.rb
@@ -75,15 +75,6 @@ ActiveRecord::Schema.define(version: 20170331103624) do
     t.index ["user_id"], name: "index_roles_users_on_user_id", using: :btree
   end
 
-  create_table "user_client_types", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "client_type_id"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-    t.index ["client_type_id"], name: "index_user_client_types_on_client_type_id", using: :btree
-    t.index ["user_id"], name: "index_user_client_types_on_user_id", using: :btree
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
     t.string   "username",               default: "",    null: false
@@ -130,8 +121,6 @@ ActiveRecord::Schema.define(version: 20170331103624) do
   add_foreign_key "oauth_access_tokens", "users", column: "resource_owner_id"
   add_foreign_key "roles_users", "roles"
   add_foreign_key "roles_users", "users"
-  add_foreign_key "user_client_types", "client_types"
-  add_foreign_key "user_client_types", "users"
   add_foreign_key "users_client_types", "client_types"
   add_foreign_key "users_client_types", "users"
 end
