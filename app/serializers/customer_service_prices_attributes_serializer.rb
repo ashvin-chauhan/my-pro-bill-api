a16ef3fe0eee56e65f644adcb6a83a6495051492
Ashvin chauhan <ashvin.tps@gmail.com>
2017-04-12 18:50:26 +0530

    Changes in customer show route

---
+++ app/serializers/customer_service_prices_attributes_serializer.rb
@@ -0,0 +1,7 @@
+class CustomerServicePricesAttributesSerializer < ActiveModel::Serializer
+  attributes :id, :price, :service_name
+
+  def service_name
+    object.client_service.try(:service_name)
+  end
+end
