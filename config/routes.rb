f2ec0cec8fa01cd0bd15e4994c8d5b165c7f8b5c
Ashvin chauhan <ashvin.tps@gmail.com>
2017-04-12 15:07:33 +0530

    Add subdomain exist or not endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
     patch "/confirm" => "user/confirmations#confirm"
   end
 
+  get "/subdomain_exist" => "users#subdomain_exist"
+
   resources :client_types, except: [:new, :edit]
 
   # Role wise users listing
