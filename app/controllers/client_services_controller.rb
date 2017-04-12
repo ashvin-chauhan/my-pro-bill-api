5ea9ff1bebb7654da137b2f9f0ba07f6c31d6b8b
vishaltps <vishalsadriya.tps@gmail.com>
2017-04-12 11:43:55 +0530

    Changes in crud of Client services

--- app/controllers/client_services_controller.rb
+++ app/controllers/client_services_controller.rb
@@ -1,9 +1,35 @@
 class ClientServicesController < ApplicationController
   include InheritAction
-  before_action :get_client, only: [:create]
+  before_action :get_client
+  before_action :get_service, only: [:show, :destroy,:update]
+
+
+  def index
+  	render json: @client.client_services
+  end
 
   def create
     @resource = @client.client_services.new(resource_params)
     super
+  end	
+
+  def update
+    super
+  end	
+
+  def show
+    render json: @resource, status: 200
+  end
+
+  def destroy
+    @resource.destroy!
+
+    head 200
+  end
+
+  private
+
+  def get_service
+    @resource = @client.client_services.find(params[:id])
   end
 end
