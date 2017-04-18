80a52e86b9bd5ce00dee0e6eba90672a45e4c2e0
Ashvin chauhan <ashvin.tps@gmail.com>
2017-04-18 17:35:51 +0530

    Optimize list invoice API queries

--- app/serializers/customer_invoices_attributes_serializer.rb
+++ app/serializers/customer_invoices_attributes_serializer.rb
@@ -1,14 +1,14 @@
 class CustomerInvoicesAttributesSerializer < ActiveModel::Serializer
-  attributes :id, :service_ticket_id, :invoice_number, :status, :amount, :genration_date, :due_date
+  attributes :id, :service_ticket_id, :invoice_number, :status, :amount, :generation_date, :due_date
   attribute :customer, :if => Proc.new { instance_options[:customer] }
   attribute :company_name, :if => Proc.new { instance_options[:service] == "true" }
   has_many :service_ticket, serializer: InvoiceServiceTicketAttributesSerializer, :if => Proc.new { instance_options[:service] == "true" }
 
   def amount
-    object.service_ticket.service_ticket_items.sum(:cost)
+    object['amount']
   end
 
-  def genration_date
+  def generation_date
     object.service_ticket.try(:service_creation_date).try(:strftime, "%m/%d/%Y")
   end
 
