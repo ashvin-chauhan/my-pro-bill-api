cac0e5e860a99a4183b31cb1d978727f8da1a22c
Ashvin chauhan <ashvin.tps@gmail.com>
2017-04-19 14:22:08 +0530

    Add public invoices in gitignore

--- app/services/common_service.rb
+++ app/services/common_service.rb
@@ -3,17 +3,6 @@ class CommonService
 
   # Class Methods
   class << self
-    def create_file(path, extension)
-      dir = File.dirname(path)
-
-      unless File.directory?(dir)
-        FileUtils.mkdir_p(dir)
-      end
-
-      path << ".#{extension}"
-      File.new(path, 'w')
-    end
-
     def create_invoice_temp_table
       Temping.create :invoice_error do
         with_columns do |t|
