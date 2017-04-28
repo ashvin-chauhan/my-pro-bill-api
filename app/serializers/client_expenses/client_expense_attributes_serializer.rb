23eda974193afe0f97200d84c1f93a85457ba50e
Divyang Hirpara <divyanghirpara@gmail.com>
2017-04-28 17:38:42 +0530

    change in serialiser

--- app/serializers/client_expenses/client_expense_attributes_serializer.rb
+++ app/serializers/client_expenses/client_expense_attributes_serializer.rb
@@ -1,7 +1,7 @@
 class ClientExpenses::ClientExpenseAttributesSerializer < ActiveModel::Serializer
   attributes :id, :client_id, :expense_name, :amount, :vendor_name, :expense_date,
              :description, :start_mileage, :end_mileage, :status, :created_by
-  belongs_to :expense_category, serializer: CategoryAttributesSerializer, :if => Proc.new { instance_options[:category] == true }
+  belongs_to :expense_category, serializer: ClientExpenses::CategoryAttributesSerializer, :if => Proc.new { instance_options[:category] == true }
   has_many :client_expense_attachments, key: "client_expense_attachments", serializer: AttchmentAttributesSerializer, :if => Proc.new { instance_options[:attachment] == true }
 
   def created_by
