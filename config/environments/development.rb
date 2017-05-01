e4cb3b9ff29585b95b4dba2c6b5bba430fae295a
Ashvin chauhan <ashvin.tps@gmail.com>
2017-05-01 12:35:11 +0530

    Change mail credentail from sendgrid to gmail credential

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -66,12 +66,12 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = true
   # config.action_mailer.delivery_method = :smtp
   # config.action_mailer.smtp_settings = {
-  #   :address => "smtp.sendgrid.net",
-  #   :port => 587,
-  #   :domain => "myprobill.com",
-  #   :authentication => :plain,
-  #   :user_name => "techplusqa",
-  #   :password => "techplus123",
+  #   :address              => "smtp.gmail.com",
+  #   :port                 => 587,
+  #   :domain               => 'gmail.com',
+  #   :user_name            => 'techplusqa@gmail.com',
+  #   :password             => 'techplus123',
+  #   :authentication       => 'plain',
   #   :enable_starttls_auto => true
   # }
 end
